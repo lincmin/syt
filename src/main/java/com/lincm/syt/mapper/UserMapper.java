f05829abe04794e85855c3bf05a88d609ee5ddcb
lincmin <lincm69696@qq.com>
2021-05-15 22:37:14 +0800

    添加mapper

---
+++ src/main/java/com/lincm/syt/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.lincm.syt.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lincm.syt.entity.User;
+
+@Repository
+public interface UserMapper extends BaseMapper<User> {
+
+}
