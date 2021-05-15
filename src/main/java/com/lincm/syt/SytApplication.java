e5aaface35af9eafacfa0d68fae3e8077701aec1
lincmin <lincm69696@qq.com>
2021-05-15 22:38:02 +0800

    启动类添加MapperScan

--- src/main/java/com/lincm/syt/SytApplication.java
+++ src/main/java/com/lincm/syt/SytApplication.java
@@ -1,9 +1,11 @@
-package com.example.demo;
+package com.lincm.syt;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.lincm.syt.mapper")
 public class SytApplication {
 
 	public static void main(String[] args) {
