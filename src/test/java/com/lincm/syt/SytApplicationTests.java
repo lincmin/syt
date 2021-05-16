20147f7f18f1fe442e62e1564bec465385dd5434
lincmin <lincm69696@qq.com>
2021-05-16 09:48:09 +0800

    添加数据

--- src/test/java/com/lincm/syt/SytApplicationTests.java
+++ src/test/java/com/lincm/syt/SytApplicationTests.java
@@ -13,6 +13,18 @@ import com.lincm.syt.mapper.UserMapper;
 class SytApplicationTests {
 	@Autowired
 	private UserMapper userMapper;
+	
+	//添加
+	@Test
+	public void testAdd() {
+		User user = new User();
+		user.setName("lily");
+		user.setAge(22);
+		user.setEmail("111@qq.com");
+		int insert = userMapper.insert(user);
+		System.out.println(insert);
+	}
+	
 	@Test
 	public void findAll() {
 		List<User> users = userMapper.selectList(null);
